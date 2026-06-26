fcaf8aff231196da621bfc6bdeb546d0e4d2a45e
Tinu <tinuan96@gmail.com>
2026-06-26 18:59:47 +0100

    Merge remote-tracking branch 'origin/PGios' into PGios
    # Conflicts:
    #	android/.idea/planningMode.xml
    #	android/app/capacitor.build.gradle
    #	android/build.gradle

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Capacitor
-import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,7 +7,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
         return true
     }
 
